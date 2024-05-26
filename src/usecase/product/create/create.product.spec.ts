1e812ce3cf3dfe17416e60bedd77e8e54cbe19a3
Victor Divino <victordoamordivino@gmail.com>
2024-05-26 15:01:12 -0300

    added create product use case tests

---
+++ src/usecase/product/create/create.product.spec.ts
@@ -0,0 +1,40 @@
+import CreateProductUseCase from "./create.product.usecase";
+
+const input = {
+    type: "a",
+    name: "xpto",
+    price: 12.55
+  };
+
+const MockRepository = () => {
+    return {
+      find: jest.fn(),
+      findAll: jest.fn(),
+      create: jest.fn(),
+      update: jest.fn(),
+    };
+  };
+
+describe("Unit test product customer use case", () => {
+    it("should create a product", async () => {
+        const productRepository = MockRepository();
+        const productCreateUseCase = new CreateProductUseCase(productRepository);
+        const output = await productCreateUseCase.execute(input);
+
+        expect(output).toEqual({
+            name: input.name,
+            price: input.price
+        })
+    });
+
+    it("should thrown an error when type is missing", async () => {
+        const productRepository = MockRepository();
+        const productCreateUseCase = new CreateProductUseCase(productRepository);
+        
+        input.type = "";
+        
+        await expect(productCreateUseCase.execute(input)).rejects.toThrow(
+            "Product type not supported"
+          );
+    })
+});
